1ac7b8b6e3d7314d1249dd4dfcfdd54c5d488192
Iftekar Hossain <iftekar.hossain123@gmail.com>
2026-04-11 14:48:00 -0400

    Making change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 import HomePage from "./home-page";
 import Footer from "./footer";
+import About from "./about";
 
 function App() {
   return (
     <div className="App">
       <MainLayout>
         <HomePage />
-
+        <About />
         <Footer />
       </MainLayout>
     </div>
