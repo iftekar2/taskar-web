f4150510594db6b26091c4eca4da3a31419b75c0
Iftekar Hossain <iftekar.hossain123@gmail.com>
2026-04-11 14:36:50 -0400

    Making update

--- src/email-confirmation.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function emailConfirmation() {
-  return <EmailConfirmation></EmailConfirmation>;
-}
-
-const EmailConfirmation = styled.div``;
-
-export default emailConfirmation;
